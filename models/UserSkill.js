e0771187265e5d2d4e09e43e62f4b3b8e675f57c
ferfer-ferfer <ferielmenouerinformatique@gmail.com>
2025-04-15 16:19:24 +0100

    UserSkill.js

--- models/UserSkill.js
+++ models/UserSkill.js
@@ -1,1 +1,13 @@
-
+module.exports = (sequelize, DataTypes) => {
+    const UserSkill = sequelize.define('UserSkill', {
+      type: {
+        type: DataTypes.ENUM('teach', 'learn'),
+        allowNull: false
+      }
+    }, {
+      tableName: 'userskills'
+    });
+  
+    return UserSkill;
+  };
+  
