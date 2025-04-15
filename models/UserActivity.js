a62f850e1c2d610ae6da6d871ea1166d36245d6b
ferfer-ferfer <ferielmenouerinformatique@gmail.com>
2025-04-15 17:24:32 +0100

    UserActivity.js

---
+++ models/UserActivity.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+    const UserActivity = sequelize.define('UserActivity', {
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      activityType: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      value: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      spEarned: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    }, {
+      tableName: 'useractivity',
+    });
+  
+    
+    return UserActivity;
+  };
